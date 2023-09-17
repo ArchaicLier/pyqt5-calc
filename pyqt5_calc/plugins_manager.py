e9a6a2a543499d9caf0ea1fbd823aa166117a865
1i3R <archaiclier@gmail.com>
2023-09-17 13:51:55 +0300

    Repalce avalible plguins

--- pyqt5_calc/plugins_manager.py
+++ pyqt5_calc/plugins_manager.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractmethod
 import importlib
 import pkgutil
-from re import I
 import typing
+import inspect
 
 import pyqt5_calc.plugins
 
@@ -40,27 +40,6 @@ class AbstractPlugin(ABC):
         """
         ...
 
-class PluginsManagerWindow(QMainWindow):
-    def __init__(self, manager, parent=None) -> None:
-        super().__init__(parent)
-
-        widget = QWidget()
-        layout = QVBoxLayout(widget)
-
-        qlist_plguins = QListWidget()
-        layout.addWidget(qlist_plguins)
-
-        item_test = QListWidgetItem('Test')
-
-        qlist_plguins.addItem(item_test)
-        qlist_plguins.itemClicked.connect(self._item_clicked)
-
-        self.setCentralWidget(widget)
-
-    def _item_clicked(self,item:QListWidgetItem):
-        print(item.text())
-
-
 class PluginsManager():
     """Plugins manager window"""
 
@@ -70,11 +49,21 @@ class PluginsManager():
 
         self.loaded_plugins = {}
 
-        self.available_plugins = {
+        self.available_modules = {
             name: importlib.import_module(pyqt5_calc.plugins.__name__+'.'+name)
             for finder, name, ispkg
             in pkgutil.iter_modules(pyqt5_calc.plugins.__path__)
         }
+
+        self.available_plugins = {} # type: typing.Dict[str, typing.Type[AbstractPlugin]]
+
+        #TODO Add PlguinItem class
+        for module_name,module in self.available_modules.items():
+            for class_name, class_obj in inspect.getmembers(module, inspect.isclass):
+                if issubclass(class_obj,AbstractPlugin) and class_obj != AbstractPlugin:
+                    self.available_plugins['{}.{}'.format(module_name,class_name)] = class_obj
+
+        print(self.available_plugins)
 
         self.window_plugins = PluginsManagerWindow(self)
         self.window_plugins.show()
@@ -83,8 +72,8 @@ class PluginsManager():
         if plugin_name in self.loaded_plugins:
             return 'Plugin already loaded'
         if plugin_name in self.available_plugins:
-            plugin = self.available_plugins[plugin_name].Plugin()# type: AbstractPlugin
 
+            plugin = self.available_plugins[plugin_name]()
             plugin.load_plugin(self.window)
 
             self.loaded_plugins[plugin_name] = plugin
@@ -93,4 +82,18 @@ class PluginsManager():
 
         return 'No named plugin'
 
+class PluginsManagerWindow(QMainWindow):
+    def __init__(self, manager: PluginsManager, parent=None) -> None:
+        super().__init__(parent)
 
+        widget = QWidget()
+        layout = QVBoxLayout(widget)
+
+        qlist_plguins = QListWidget()
+        layout.addWidget(qlist_plguins)
+        qlist_plguins.itemClicked.connect(self._item_clicked)
+
+        self.setCentralWidget(widget)
+
+    def _item_clicked(self,item:QListWidgetItem):
+        print(item.text())
